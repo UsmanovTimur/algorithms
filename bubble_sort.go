33b2a8e5f3da274067ae6051ad2e80ea3b4f072c
Тимур Усманов <usmanov_t@ufanet.ru>
2020-05-05 16:56:32 +0500

    buble sort

---
+++ bubble_sort.go
@@ -0,0 +1,22 @@
+package main
+
+// Сортировка пузырьком
+func main() {
+	arr := []int{9, 9, 8, 7, 7, 6, 6, 5, 5, 4, 3, 2, 1, 10}
+	arr = bubleSort(arr)
+	for i := 0; i < len(arr); i++ {
+		print(arr[i], ",")
+	}
+}
+func bubleSort(a []int) []int {
+	for j := 1; j < len(a); j++ {
+		for i := 0; i < len(a)-j; i++ {
+			if a[i] > a[i+1] {
+				a[i] = a[i] + a[i+1]
+				a[i+1] = a[i] - a[i+1]
+				a[i] = a[i] - a[i+1]
+			}
+		}
+	}
+	return a
+}
